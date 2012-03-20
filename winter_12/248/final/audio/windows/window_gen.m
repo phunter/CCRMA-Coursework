d816b5012a42af77a611f15f7e352179090c9866
P. Hunter McCurry <p.hunter.mccurry@gmail.com>
2012-03-20 05:30:30 -0700

    new audio window

--- winter_12/248/final/audio/windows/window_gen.m
+++ winter_12/248/final/audio/windows/window_gen.m
@@ -3,6 +3,11 @@
 fs = 44100;
 N = 2048;
 
-Y = hanning(N);
+Y = hanning(N/2);
+Y_shift = fftshift(Y);
 
-wavwrite(Y,fs,'hanning.wav');
+Y_insert = [ ones(N/4,1); Y_shift ; ones(N/4,1)];
+Y_reshift = fftshift(Y_insert);
+
+% plot(Y_reshift);
+wavwrite(Y_reshift,fs,'hanning_extended.wav');
